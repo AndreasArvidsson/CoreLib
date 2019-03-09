3d5a85a87d0186db0a2eabaee6f32c762e9c48f0
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2019-03-09 10:44:22 +0100

    Added cpp tile to os namepsace

---
+++ OS.cpp
@@ -0,0 +1,91 @@
+#include "OS.h"
+#include <xmmintrin.h> //_MM_SET_FLUSH_ZERO_MODE
+#include "Error.h"
+
+void OS::setPriorityHigh() {
+    setPriority(HIGH_PRIORITY_CLASS);
+}
+
+void OS::setPriority(const int value) {
+    if (!SetPriorityClass(GetCurrentProcess(), value)) {
+        throw Error("Failed to set process priority(%d)", GetLastError());
+    }
+}
+
+void OS::flushDenormalizedZero() {
+    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+}
+
+void OS::minimizeWindow() {
+    ShowWindow(GetConsoleWindow(), SW_MINIMIZE);
+}
+
+void OS::hideWindow() {
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+}
+
+void OS::showWindow() {
+    ShowWindow(GetConsoleWindow(), SW_RESTORE);
+}
+
+void OS::showInForeground() {
+    SetForegroundWindow(GetConsoleWindow());
+}
+
+const bool OS::isWindowMinimized() {
+    return IsIconic(GetConsoleWindow());
+}
+
+const std::string OS::getExePath() {
+    char value[1024];
+    int length = GetModuleFileNameA(NULL, value, sizeof(value));
+    if (length == 0) {
+        throw Error("Failed to get executable path: %d", GetLastError());
+    }
+    return std::string(value);
+}
+
+const std::string OS::getExeDirPath() {
+    std::string exePath = getExePath();
+    size_t i = exePath.rfind('\\');
+    if (i != std::string::npos) {
+        return exePath.substr(0, i + 1);
+    }
+    return exePath;
+}
+
+const bool OS::regValueExists(const HKEY hKey, const std::string &path, const std::string &name) {
+    const LSTATUS status = RegGetValue(hKey, path.c_str(), name.c_str(), RRF_RT_ANY, NULL, nullptr, nullptr);
+    switch (status) {
+    case ERROR_SUCCESS:
+        return true;
+    case ERROR_FILE_NOT_FOUND:
+        return false;
+    default:
+        throw Error("Failed to check if register key value: %d", status);
+    }
+}
+
+const std::string OS::regGetValue(const HKEY hKey, const std::string &path, const std::string &name) {
+    char value[1024];
+    DWORD length;
+    const LSTATUS status = RegGetValue(hKey, path.c_str(), name.c_str(), RRF_RT_ANY, NULL, value, &length);
+    if (status != ERROR_SUCCESS) {
+        throw Error("Failed to get register value: %d", status);
+    }
+    return std::string(value);
+}
+
+void OS::regSetValue(const HKEY hKey, const std::string &path, const std::string &name, const std::string &value) {
+    const LSTATUS status = RegSetKeyValueA(hKey, path.c_str(), name.c_str(), REG_SZ, value.c_str(), (DWORD)value.size());
+    if (status != ERROR_SUCCESS) {
+        throw Error("Failed to set register value: %d", status);
+    }
+}
+
+void OS::regRemoveValue(const HKEY hKey, const std::string &path, const std::string &name) {
+    const LSTATUS status = RegDeleteKeyValueA(hKey, path.c_str(), name.c_str());
+    if (status != ERROR_SUCCESS) {
+        throw Error("Failed to remove register value: %d", status);
+    }
+}
--- OS.h
+++ OS.h
@@ -2,98 +2,25 @@
 #define NOMINMAX
 #include <windows.h>
 #include <string>
-#include <xmmintrin.h> //_MM_SET_FLUSH_ZERO_MODE
-#include "Error.h"
 
-class OS {
-public:
+namespace OS {
 
-	static void setPriorityHigh() {
-		setPriority(HIGH_PRIORITY_CLASS);
-	}
+    void setPriorityHigh();
+    void setPriority(const int value);
+    void flushDenormalizedZero();
 
-	static void setPriority(const int value) {
-		if (!SetPriorityClass(GetCurrentProcess(), value)) {
-			throw Error("Failed to set process priority(%d)", GetLastError());
-		}
-	}
+    void minimizeWindow();
+    void hideWindow();
+    void showWindow();
+    void showInForeground();
+    const bool isWindowMinimized();
 
-	static void minimizeWindow() {
-		ShowWindow(GetConsoleWindow(), SW_MINIMIZE);
-	}
+    const std::string getExePath();
+    const std::string getExeDirPath();
 
-	static void hideWindow() {
-		ShowWindow(GetConsoleWindow(), SW_HIDE);
-	}
-
-	static void showWindow() {
-		ShowWindow(GetConsoleWindow(), SW_RESTORE);
-	}
-
-    static void showInForeground() {
-        SetForegroundWindow(GetConsoleWindow());
-    }
-
-	static const boolean isWindowMinimized() {
-		return IsIconic(GetConsoleWindow());
-	}
-
-	static void flushDenormalizedZero() {
-		_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-	}
-
-	static const std::string getExePath() {
-		char value[1024];
-		int length = GetModuleFileNameA(NULL, value, sizeof(value));
-		if (length == 0) {
-			throw Error("Failed to get executable path: %d", GetLastError());
-		}
-		return std::string(value);
-	}
-
-	static const std::string getExeDirPath() {
-		std::string exePath = getExePath();
-		size_t i = exePath.rfind('\\');
-		if (i != std::string::npos) {
-			return exePath.substr(0, i + 1);
-		}
-		return exePath;
-	}
-
-	static const bool regValueExists(const HKEY hKey, const std::string &path, const std::string &name) {
-		const LSTATUS status = RegGetValue(hKey, path.c_str(), name.c_str(), RRF_RT_ANY, NULL, nullptr, nullptr);
-		switch (status) {
-		case ERROR_SUCCESS:
-			return true;
-		case ERROR_FILE_NOT_FOUND:
-			return false;
-		default:
-			throw Error("Failed to check if register key value: %d", status);
-		}
-	}
-
-	static const std::string regGetValue(const HKEY hKey, const std::string &path, const std::string &name) {
-		char value[1024];
-		DWORD length;
-		const LSTATUS status = RegGetValue(hKey, path.c_str(), name.c_str(), RRF_RT_ANY, NULL, value, &length);
-		if (status != ERROR_SUCCESS) {
-			throw Error("Failed to get register value: %d", status);
-		}
-		return std::string(value);
-	}
-
-	static void regSetValue(const HKEY hKey, const std::string &path, const std::string &name, const std::string &value) {
-		const LSTATUS status = RegSetKeyValueA(hKey, path.c_str(), name.c_str(), REG_SZ, value.c_str(), (DWORD)value.size());
-		if (status != ERROR_SUCCESS) {
-			throw Error("Failed to set register value: %d", status);
-		}
-	}
-
-	static void regRemoveValue(const HKEY hKey, const std::string &path, const std::string &name) {
-		const LSTATUS status = RegDeleteKeyValueA(hKey, path.c_str(), name.c_str());
-		if (status != ERROR_SUCCESS) {
-			throw Error("Failed to remove register value: %d", status);
-		}
-	}
+    const bool regValueExists(const HKEY hKey, const std::string &path, const std::string &name);
+    const std::string regGetValue(const HKEY hKey, const std::string &path, const std::string &name);
+    void regSetValue(const HKEY hKey, const std::string &path, const std::string &name, const std::string &value);
+    void regRemoveValue(const HKEY hKey, const std::string &path, const std::string &name);
 
 };
